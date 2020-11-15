39003b2dfab9eedec6d497a65675b587846e3d10
Mariano <elvillano79@yahoo.com>
2020-11-15 23:06:40 +0000

    Pagina principal en ingles y portugues

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+//Acuse de recibo
 console.log("Conectado!")
+//Llamado de menú al tocar el botón menu-btn 
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.menu-pr').classList.toggle('show');
 });
+//Llamado de rutinas scroll reveal
 ScrollReveal().reveal('.contenedor');
 ScrollReveal().reveal('.nueva-entrada');
+//Retrasos de 1 segundo
 ScrollReveal().reveal('.art-cards', { delay: 1000 });
 ScrollReveal().reveal('.social', { delay: 1000 });
 ScrollReveal().reveal('.hmundo', { delay: 1000 });
