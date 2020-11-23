afe46df54f91030dee09d3c42d3062c3cd5b1b3b
Mariano <elvillano79@yahoo.com>
2020-11-23 22:43:56 +0000

    Definicion de formatos de los articulos

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //Acuse de recibo
-console.log("Conectado!")
+console.log("Conectado! - Connected! - Ligado! - Collegato!")
 //Llamado de menú al tocar el botón menu-btn 
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.menu-pr').classList.toggle('show');
 });
-//Llamado de rutinas scroll reveal
-ScrollReveal().reveal('.contenedor');
-ScrollReveal().reveal('.nueva-entrada');
-//Retrasos de 1 segundo
+//Retrasos de 1 segundo con ScrollReveal
 ScrollReveal().reveal('.art-cards', { delay: 1000 });
 ScrollReveal().reveal('.social', { delay: 1000 });
 ScrollReveal().reveal('.hmundo', { delay: 1000 });
 ScrollReveal().reveal('.cont-footer', { delay: 1000 });
+//Retrasos de 500 milisegundos
+ScrollReveal().reveal('.contenido', { delay: 500 });
+ScrollReveal().reveal('.divconsola', { delay: 500 });
+ScrollReveal().reveal('.img-mini-simple',{delay:500});
